ecabf6f7cbac4909dbddf08c185f85d5ef1068af
Ricardo Belmonte <Belmonte90@gmail.com>
2021-02-19 14:47:05 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+<head>
+  <link href="{{ asset('css/style_home.css') }}" rel="stylesheet" media="screen">
+</head>
+@section('content')
+
+    <img src="/images/portada.jpg" alt="Portada"></img>
+    <div class="container">
+      <div>
+        <div class="flex-grid-2">
+        	<section class="panel"><p>Cientos de años de tradición se esconden detrás de los cubiertos, tablas y herramientas de cocina construidos de forma totalmente artesanal. También juguetes tradicionales.</p></section>
+        	<section class="panel"><p>El boj común es de hábito arbustivo o arbóreo; alcanza excepcionalmente los 12 m de altura, con un tallo muy ramificado cubierto de una corteza lisa en los ejemplares jóvenes, surcada de resquebrajaduras en los adultos, de color pardogrisáceo. Las hojas son lanceoladas a ovadas o elípticas, opuestas, coriáceas, de color verde oscuro por el haz y más claro en el envés, de hasta 30 mm, con el borde algo curvado hacia abajo. Las flores son monoicas, con masculinas y femeninas presentes en el mismo ejemplar; aparecen a comienzos de primavera, en pequeñas inflorescencias de varias flores masculinas y una femenina en las axilas foliares. Miden unos 2 mm, de color amarillo, poco vistosas, carentes de corola, sin fragancia pero ricas en néctar, que atrae a himenópteros y dípteros. El fruto es una cápsula marrón o gris, coriácea, de alrededor de 1 cm de largo, que contiene numerosas semillas.</p></section>
+        </div>
+    	</div>
+
+
+
+    </div>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Lemon Hill') }}</title>
+
+    <!-- Scripts -->
+    <!--
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    -->
+
+
+    <!-- Fonts -->
+    <!--
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    -->
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Yesteryear&display=swap" rel="stylesheet">
+    <!-- Styles -->
+    <!--
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  -->
+  <script src="{{ asset('js/jquery.js') }}" type="text/javascript"></script>
+  <link href="{{ asset('css/components.css') }}" rel="stylesheet" media="screen">
+  <link href="{{ asset('css/screen.css') }}" rel="stylesheet" media="screen">
+
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+  <nav id="site-nav" class="nav-smart">
+    <div>
+      <input type="checkbox" id="mobnav" name="mobnav" aria-hidden="true" hidden>
+      <strong><label for="mobnav"><span class="symbol-list"></span><mark></mark></label></strong>
+      <div hidden>
+      <a class="site-logo" href="{{ url('/home')}}" title="Coberts de Boix">
+        <img src="/images/logo.jpg" alt="Lemon Hill Logo" class="image-logo"><b>Coberts de Boix</b>
+      </a>
+      <a href="{{ url('/home')}}" title="Home" style="margin-left:275px;">Home</a>
+      <a href="{{ url('/onlineShop')}}" title="Online Shop">Online Shop</a>
+      @guest
+
+
+
+            @if (Route::has('login'))
+
+            <a href="{{ route('login') }}">{{ __('Login') }}</a>
+
+            @endif
+
+            @if (Route::has('register'))
+
+            <a href="{{ route('register') }}">{{ __('Register') }}</a>
+
+            @endif
+
+        @else
+            <a href="{{ route('logout') }}"  onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
+                                               <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                    @csrf
+                                                </form>
+
+      @endguest
+
+
+
+     </div>
+   </div>
+  </nav>
+    <main id="page-main">
+
+
+      @if (Auth::check() && Auth::user()->admin==true)
+      <hr>
+      <nav id="page-nav" class="nav-scroll">
+      <div>
+        <a href="/product_list_admin" title="Product List">Product List</a>
+        <a href="/product_creation_form" title="Create Product">Create Product</a>
+        <a href="/list_orders" title="Orders">Orders</a>
+       </div>
+       </nav>
+       <hr>
+       @endif
+@yield('content')
+
+</main>
+
+</body>
+<footer id="page-footer">
+
+    <div class="footer_label">
+      <label>Siguenos en: </label>
+    </div>
+    
+      <a href="http://facebook.com"><img src="/images/logo_facebook.png" alt="Facebook" class="icons" ></a>
+      <a href="www.instagram.com"><img src="/images/icono_instagram.png" alt="Instagram" class="icons"></a>
+
+    <div id="author">
+      <label>Creada por Ricardo Belmonte</label>
+    </div>
+    <div class="footer_label">
+      <label>Basada en Barcelona CSS y Laravel</label>
+    </div>
+
+</footer>
+</html>
